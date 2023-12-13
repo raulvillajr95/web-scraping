29e082ae1ceefea5402667194ec942cacb17821d
Raul <raulvillajr95@gmail.com>
2023-12-13 12:10:56 -0800

    print most outer tags of html file

--- web-scraping/web-scraping/main.cpp
+++ web-scraping/web-scraping/main.cpp
@@ -12,6 +12,23 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output)
    return totalSize;
 }
 
+void parseAndPrintHTML(const string& htmlContent)
+{
+   GumboOutput* output = gumbo_parse(htmlContent.c_str());
+   
+   GumboNode* root = output->root;
+   for (unsigned int i = 0; i < root->v.element.children.length; ++i)
+   {
+      GumboNode* child = static_cast<GumboNode*>(root->v.element.children.data[i]);
+      if (child->type == GUMBO_NODE_ELEMENT)
+      {
+         cout << "Tag: " << gumbo_normalized_tagname(child->v.element.tag) << endl;
+      }
+   }
+   
+   gumbo_destroy_output(&kGumboDefaultOptions, output);
+}
+
 int main()
 {
    CURL* curl = curl_easy_init();
@@ -28,15 +45,16 @@ int main()
    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &htmlContent);
    
-   // ...
-   
    CURLcode res = curl_easy_perform(curl);
-   
    if (res != CURLE_OK)
    {
       cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
       return 1;
    }
+   
+   parseAndPrintHTML(htmlContent);
+   
+   curl_easy_cleanup(curl);
    
    return 0;
 }
