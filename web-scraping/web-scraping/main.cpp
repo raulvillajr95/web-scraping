a19be290165f04d278fd6e2002e5e39cc04b7466
Raul <raulvillajr95@gmail.com>
2023-12-13 11:45:24 -0800

    Settle curl & gumbo disputes

--- web-scraping/web-scraping/main.cpp
+++ web-scraping/web-scraping/main.cpp
@@ -1,14 +1,42 @@
-//
-//  main.cpp
-//  web-scraping
-//
-//  Created by Raul Villalobos on 12/12/23.
-//
-
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include <gumbo.h>
 
-int main(int argc, const char * argv[]) {
-   // insert code here...
-   std::cout << "Hello, World!\n";
+using namespace std;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output)
+{
+   size_t totalSize = size * nmemb;
+   output->append(static_cast<char*>(contents), totalSize);
+   return totalSize;
+}
+
+int main()
+{
+   CURL* curl = curl_easy_init();
+   if (!curl)
+   {
+      cerr << "Error initializing libcurl" << endl;
+      return 1;
+   }
+   
+   curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+   
+   string htmlContent;
+   
+   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &htmlContent);
+   
+   // ...
+   
+   CURLcode res = curl_easy_perform(curl);
+   
+   if (res != CURLE_OK)
+   {
+      cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+      return 1;
+   }
+   
    return 0;
 }
